69d856962c2c8cdd659de210714ec088e8e04d16
James <jwblangley@gmail.com>
2019-05-22 20:53:55 +0100

    Write test app to check performance of execution on large image directories
    Use of parallel stream gives a big performance boost.

--- src/App.java
+++ src/App.java
@@ -1,6 +1,29 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class App {
 
   public static void main(String[] args) {
+    File testImages = new File("exampleImages/");
+    assert testImages.isDirectory();
+
+    List<Tile> tiles = Arrays.stream(testImages.listFiles()).parallel()
+        .map(file -> {
+          try {
+            System.out.println(file.getPath());
+            return Tile.ofImage(3, file);
+          } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+            return null;
+          }
+        })
+        .collect(Collectors.toList());
+
+    System.out.println("tiles.get(0) = " + tiles.get(0).getSubtiles()[0][0]);
 
   }
 }
