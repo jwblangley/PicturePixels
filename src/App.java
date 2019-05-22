c686f18d02fce1f7e5767038408ce0aba63791d2
James <jwblangley@gmail.com>
2019-05-22 22:57:11 +0100

    WIP: Begin App flow to build tiles from target image

--- src/App.java
+++ src/App.java
@@ -1,38 +1,87 @@
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import javax.imageio.ImageIO;
 
 public class App {
 
+  private static final int NUM_SUBTILES = 3;
+  private static final int TILE_SIZE = 30;
+
+  private static final String INPUT_DIRECTORY = "exampleImages/";
+  private static final String TARGET_IMAGE = "target.png";
+
+
+  // TODO: refactor into cleaner code
+
   public static void main(String[] args) {
-    File testImages = new File("exampleImages/");
-    assert testImages.isDirectory();
+
+    // Generate target tiles
+
+    BufferedImage targetImage = null;
+    try {
+      targetImage = ImageIO.read(new File(TARGET_IMAGE));
+    } catch (IOException e) {
+      // TODO: handle exception
+      e.printStackTrace();
+    }
+
+    assert targetImage.getWidth() > TILE_SIZE * NUM_SUBTILES : "Input image is not large enough for specified tile layout";
+    assert targetImage.getHeight() > TILE_SIZE * NUM_SUBTILES : "Input image is not large enough for specified tile layout";
+
+    int tileWidth = targetImage.getWidth() / TILE_SIZE;
+    int tileHeight = targetImage.getHeight() / TILE_SIZE;
+
+
+
+    List<Tile> targetTiles = new LinkedList<>();
+    for (int y = 0; y < targetImage.getHeight() / TILE_SIZE; y++) {
+      for (int x = 0; x < targetImage.getWidth() / TILE_SIZE; x++) {
+        BufferedImage subImage = targetImage.getSubimage(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+
+        // TODO: create tile
+        // TODO: add tile to targetTiles
+      }
+    }
+
+    // Generate input tiles
+
+    File testImages = new File(INPUT_DIRECTORY);
+    assert testImages.isDirectory(): "Must be given a directory";
 
     int numFiles = testImages.listFiles().length;
     AtomicInteger progress = new AtomicInteger(0);
 
-    List<Tile> tiles = Arrays.stream(testImages.listFiles())
+    HashMap<Tile, File> tileSource = new HashMap<>();
+
+    List<Tile> inputTiles = Arrays.stream(testImages.listFiles())
         .parallel()
         .map(file -> {
           try {
             System.out.println(progress.incrementAndGet() + "/" + numFiles);
-            return Tile.ofImage(3, file);
+            Tile tile = Tile.ofImageFile(NUM_SUBTILES, file);
+            tileSource.put(tile, file);
+            return tile;
           } catch (IOException e) {
             return Tile.nullTile();
           }
         })
         .collect(Collectors.toList());
 
-    // Remove any null tiles (From non image files)
-    // N.B do the check this way round to avoid having to do repeat IO.
-    tiles = tiles.stream().filter(Predicate.not(Tile::isNull)).collect(Collectors.toList());
+    // Remove any null tiles (from non image files)
+    // N.B do the check this way round to avoid having to do repeat IO
+    inputTiles = inputTiles.stream().filter(Predicate.not(Tile::isNull)).collect(Collectors.toList());
 
-    System.out.println("Size: " + tiles.size());
 
+    // Test
+    System.out.println("Size: " + inputTiles.size());
 
   }
 }
